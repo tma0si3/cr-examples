8fe8512dc7ad0c66eb45e16e4c7d0cbb5e9907df
Developer <>
2015-09-01 11:40:17 +0200

    adjusted to the new api

--- cr-integration-api-examples/src/main/java/com/bosch/cr/integration/examples/Examples.java
+++ cr-integration-api-examples/src/main/java/com/bosch/cr/integration/examples/Examples.java
@@ -8,6 +8,7 @@ import org.slf4j.LoggerFactory;
 import com.bosch.cr.integration.IntegrationClientConfiguration;
 import com.bosch.cr.integration.IntegrationClient;
 import com.bosch.cr.integration.IntegrationClientImpl;
+import com.bosch.cr.integration.ThingHandle;
 import com.bosch.cr.integration.ThingIntegration;
 import com.bosch.cr.integration.messaging.ProviderConfiguration;
 import com.bosch.cr.integration.messaging.stomp.StompProviderConfiguration;
@@ -36,47 +37,50 @@ public class Examples
 
       final IntegrationClient integrationClient = IntegrationClientImpl.newInstance(integrationClientConfiguration);
 
+      /* Create a new thing integration */
+      final ThingIntegration thingIntegration = integrationClient.things();
+
       /* Create a new thing and define handlers for success and failure */
-      integrationClient.createThing("myThing")
+      thingIntegration.create("myThing")
          .onSuccess( thing -> LOGGER.info("Thing created: {}", thing))
          .onFailure(throwable -> LOGGER.error("Create Thing Failed: {}", throwable)).apply();
 
       /* Register for lifecycle events of *all* things */
       final String allThings_lifecycleRegistration = "allThings_lifecycleRegistration";
-      integrationClient.registerForThingLifecycleEvent(allThings_lifecycleRegistration,
+      thingIntegration.registerForLifecycleEvent(allThings_lifecycleRegistration,
          lifecycle -> LOGGER.info("lifecycle received: {}", lifecycle));
 
       /* Register for *all* attribute changes of *all* things */
       final String allThings_attributeChangeRegistration = "allThings_attributeChangeRegistration";
-      integrationClient.registerForThingAttributeChange(allThings_attributeChangeRegistration,
+      thingIntegration.registerForAttributeChange(allThings_attributeChangeRegistration,
          change -> LOGGER.info("attributeChange received: {}", change));
 
       /* Register for *specific* attribute changes of *all* things */
       final String allThings_specificAttributeChangeRegistration = "allThings_specificAttributeChangeRegistration";
-      integrationClient.registerForThingAttributeChange(allThings_specificAttributeChangeRegistration, "address/city",
+      thingIntegration.registerForAttributeChange(allThings_specificAttributeChangeRegistration, "address/city",
          change -> LOGGER.info("attributeChange received: {}", change));
 
       /* Terminate a registration using the client */
-      integrationClient.deregister(allThings_lifecycleRegistration);
+      thingIntegration.deregister(allThings_lifecycleRegistration);
 
       /*--------------------------------------------------------------------------------------------------------------*/
 
       /* Create a handle for an existing thing */
-      final ThingIntegration myThing = integrationClient.forThing("myThing");
+      final ThingHandle myThing = thingIntegration.forId("myThing");
 
       /* Register for *all* lifecycle events of a *specific* thing */
       final String myThing_lifecycleRegistration = "myThing_lifecycleRegistration";
-      myThing.registerForThingLifecycleEvent(myThing_lifecycleRegistration,
+      myThing.registerForLifecycleEvent(myThing_lifecycleRegistration,
          lifecycleEvent -> LOGGER.info("lifecycle received: {}", lifecycleEvent));
 
       /* Register for *all* attribute changes of a *specific* thing */
       final String myThing_attributeChangeRegistration = "myThing_attributeChangeRegistration";
-      myThing.registerForThingAttributeChange(myThing_attributeChangeRegistration,
+      myThing.registerForAttributeChange(myThing_attributeChangeRegistration,
          change -> LOGGER.info("attributeChange received: {}", change));
 
       /* Register for *specific* attribute changes of a *specific* thing */
       final String myThing_specificAttributeChangeRegistration = "myThing_specificAttributeChangeRegistration";
-      myThing.registerForThingAttributeChange(myThing_specificAttributeChangeRegistration, "address/city",
+      myThing.registerForAttributeChange(myThing_specificAttributeChangeRegistration, "address/city",
          change -> LOGGER.info("attributeChange received: {}", change));
 
       /* Create new attribute for a thing and define handlers for success and failure */
@@ -89,7 +93,7 @@ public class Examples
       myThing.deregister(myThing_lifecycleRegistration);
 
       /* Delete a thing */
-      myThing.deleteThing();
+      myThing.delete();
 
       /* Destroy the client and wait 30 seconds for its graceful shutdown */
       integrationClient.destroy(30, TimeUnit.SECONDS);
